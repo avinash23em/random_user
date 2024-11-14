3770a07808f4d98f77e9a2647868eed5838a5b96
avinash23em <ap23vi@gmail.com>
2024-11-14 23:26:19 +0530

    intial

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -1,6 +1,33 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
+
+
+
+
 export default function Random(){
-return
-<div>
-    
+const[users,setUsers]=useState([]);
+const[page,setpage]=useState('');
+const[loading,setloading]=useState(false);
+
+useEffect(() => {
+    const fetchUsers = async () => {
+      setloading(true);
+      try {
+        const response = await axios.get(`https://randomuser.me/api/?page=${page}&results=5`);
+        setUsers((prevUsers) => [...prevUsers, ...response.data.results]);
+      } catch (error) {
+        console.error("Error fetching users", error);
+      }
+      setloading(false);
+    };
+    fetchUsers();
+  }, [page]);
+  
+  return (
+  <div>
+
+
+<button onClick={(setpage+1)}>click for more users</button>
 </div>
+);
 }
